8c7316389063e9f83edbea1611b70debf4733e2e
Radyslav_Shovkivskyi <r.shovkivskyi@gmail.com>
2022-12-01 16:27:05 +0200

    Added clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import VideoBg from './assets/video.mp4'
-
+import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown' //import clock
+import '@leenguyen/react-flip-clock-countdown/dist/index.css' // clock css
+import './index.css'
 
 
 
@@ -12,6 +14,13 @@ export const App = () => {
       <div className='pageContent'>
         <h1>Launching Soon</h1>
         <h3>Leave your Email and we'll let you know once the site goes live.</h3>
+        <FlipClockCountdown 
+      to={new Date().getTime() + 24 * 3600 * 1000 + 5000}
+      className='flip-clock'
+      labels={['DAYS', 'HOURS', 'MINUTES', 'SECONDS']}
+      duration={0.5}
+      />
+      <button className='btn'>Notify Me</button>
       </div>
     </section>
   )
