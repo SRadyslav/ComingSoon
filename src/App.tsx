14ba0ff7d945129df2a554264040aebe90b9c964
Radyslav_Shovkivskyi <r.shovkivskyi@gmail.com>
2022-12-01 15:32:01 +0200

    Created page and styles to it

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import VideoBg from './assets/video.mp4'
+
+
+
 
 export const App = () => {
   return (
-    <div>App</div>
+    <section className='page'>
+      <div className='overlay'></div>
+      <video src={VideoBg} autoPlay muted loop ></video>
+      <div className='pageContent'>
+        <h1>Launching Soon</h1>
+        <h3>Leave your Email and we'll let you know once the site goes live.</h3>
+      </div>
+    </section>
   )
 }
